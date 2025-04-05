1d91c88329a6e9277f11dd5193d084825005c322
KattAtencio <n00275245@upn.pe>
2025-04-04 23:00:05 -0500

    Se agrega el ejercicio2

---
+++ TEPROO_S11_Ejercicio2/src/Alumno.java
@@ -0,0 +1,14 @@
+
+public class Alumno {
+	//Atributos miembro
+	String codigo;
+	String nombre;
+	int nota1;
+	int nota2;
+	
+	//Métodos miembro
+	double calcularPromedio() {
+		return (nota1 + nota2) / 2.0;
+	}
+
+}
